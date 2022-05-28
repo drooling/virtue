789df80db3dfb5b989787904c22bf12bcb1e6224
Surtains <97707885+drooling@users.noreply.github.com>
2022-05-28 01:43:23 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name = 'virtue-git',
-    version = '1.3.1',
+    version = '1.3.2',
     description = 'Gather information about a GitHub account',
     author = 'Surtains',
     author_email = "surtains@riseup.net",
